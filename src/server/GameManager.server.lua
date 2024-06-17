e30dfb9fb75da3adddc73de0a9a3a98ddb85ada4
anj0la <anjola.aina@gmail.com>
2024-06-17 13:45:11 -0600

    added game init to add remote events and bindable events folder to game when first loaded.

--- src/server/GameManager.server.lua
+++ src/server/GameManager.server.lua
@@ -1,1 +1,14 @@
-print('hi')
+-- Services
+local ServerScriptService = game:GetService('ServerScriptService')
+
+-- Module Scripts
+local GameModules = ServerScriptService.Server:WaitForChild('GameModules')
+local GameInit = require(GameModules:WaitForChild('GameInit'))
+
+-- Initialization (adding remote events and bindable events folder to required services)
+GameInit.init()
+
+-- Main game loop
+while task.wait() do
+	task.wait()
+end
