6fad7ae3c5d081b6026a8c829f80daa31c4799b1
anj0la <anjola.aina@gmail.com>
2024-06-26 10:23:27 -0600

    added code to change the activate game to game completed when finished.

--- src/client/MinigameScripts/DisplayKeyboxGame.client.lua
+++ src/client/MinigameScripts/DisplayKeyboxGame.client.lua
@@ -22,14 +22,40 @@ local keyboxObj = nil -- going to be an array of keyboxes probably
 
 -- Local Functions --
 
+-- Local function to change the activated game text
+local function changeActivateGameText()
+    local activateGame = keyboxObj:WaitForChild('ActivateGame')
+    local activateGameGui = activateGame:WaitForChild('ActivateGameGui')
+    local baseFrame = activateGameGui:WaitForChild('BaseFrame')
+    local activateGameLabel = baseFrame:WaitForChild('ActivateGameLabel')
+    local uiGradient = activateGameLabel:WaitForChild('UIGradient')
+    local uiStroke = activateGameLabel:WaitForChild('UIStroke')
+
+    -- colours for the gradient and stroke
+    local c0 = Color3.new(0, 1, 0)
+    local c1 = Color3.new(0, 0.12, 0)
+    local c2 = Color3.new(0, 0.47, 0)
+
+    -- changing the text to Game Completed
+    activateGameLabel.Text = 'GAME COMPLETED'
+
+    -- changing the ui gradient and stroke
+    uiGradient.Color = ColorSequence.new(c0, c1)
+    uiStroke.Color = c2
+
+end
+
 -- Local function to display the keyboard game gui
 local function onKeyboxEvent(keybox, showGui)
     KeyboxGameGui.Enabled = showGui
     keyboxObj = keybox
 end
 
--- Local function to increase the key count
+-- Local function to allow the key to be collected
 local function onButtonClicked()
+    KeyboxGameGui.Enabled = false -- disable the game gui since the game has been completed
+    -- changing the text to indicate that its been solved
+    changeActivateGameText()
     CompletedKeyboxGui:FireServer(keyboxObj)
 end
 
