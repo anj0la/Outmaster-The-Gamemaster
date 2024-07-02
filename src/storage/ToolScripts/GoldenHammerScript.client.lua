9d27dbee09428c5d0521308ee290f0344d47d67b
anj0la <anjola.aina@gmail.com>
2024-07-01 20:50:56 -0600

    finished adding tool anmiation. the hit player does work, but havent tested event.

--- src/storage/ToolScripts/GoldenHammerScript.client.lua
+++ src/storage/ToolScripts/GoldenHammerScript.client.lua
@@ -7,11 +7,10 @@ local Player = Players.LocalPlayer
 
 -- Local Variables --
 local goldenHammer = script.Parent
+local hammerHead = goldenHammer:WaitForChild('Head')
 local debounce = false
 local canDamage = false
 local swingTrack = nil
-local r6AnimationId = 'rbxassetid://18240152719' 
-local r15AnimationId = 'rbxassetid://18239965862'
 
 -- Remote Events --
 local RemoteEvents = ReplicatedStorage.Shared:WaitForChild('RemoteEvents')
@@ -19,59 +18,77 @@ local GoldenHammerDamageEvent = RemoteEvents:WaitForChild('GoldenHammerDamageEve
 
 -- Local Functions --
 
--- Local function to load the animation depending on whether the player is an R6 / R15 rig
+-- Local function to load the animation depending on whether the player is an R15 / R6 rig
 local function onEquipped()
     local character = Player.Character
+    local humanoid = character:FindFirstChildOfClass('Humanoid')
+    local animator = humanoid:FindFirstChildOfClass('Animator') or humanoid:WaitForChild('Animator')
+    local animation = Instance.new('Animation')
     
-    local Humanoid = character:FindFirstChildOfClass('Humanoid')
-    local Animator = Humanoid:FindFirstChildOfClass('Animator') or Humanoid:WaitForChild('Animator')
-    
-    local Animation = Instance.new('Animation')
-    
-    if Humanoid.RigType == Enum.HumanoidRigType.R15 then
-        Animation.AnimationId = r15AnimationId
+    if humanoid.RigType == Enum.HumanoidRigType.R15 then
+        animation = goldenHammer:FindFirstChild('Swing')
     else
-        Animation.AnimationId = r6AnimationId
+        animation = goldenHammer:FindFirstChild('SwingR6')
     end
     
-    swingTrack = Animator:LoadAnimation(Animation)
+    swingTrack = animator:LoadAnimation(animation)
+end
+
+-- Local function to check if we've hit a humanoid
+local function didHitHumanoid(hit)
+	-- we check to see if we've hit a body part (e.g., Head, LeftFoot), in which the parent is the Character model
+	if hit.Parent:FindFirstChild('Humanoid') and hit.Parent:FindFirstChild('HumanoidRootPart') then
+		return hit.Parent -- the parent is the Character model of the player that was hit
+
+	-- otherwise, we have hit an accessory (e.g., a hat), and the Character model is the parent of the accessory
+	elseif hit.Parent.Parent:FindFirstChild('Humanoid') and hit.Parent.Parent:FindFirstChild('HumanoidRootPart') then
+		return hit.Parent.Parent -- the parent of the accessory (the parent of the part hit) is the Character model
+	
+    else
+		return nil -- we did not hit another player, so return nil
+	end
+	
 end
 
 -- Local function to handle collision event
 local function onHit(hit)
-    if canDamage and hit.Parent:FindFirstChild('Humanoid') and hit.Parent:FindFirstChild('HumanoidRootPart') then
-        canDamage = false
-        local hitPlayer = Players:GetPlayerFromCharacter(hit.Parent)
-        if hitPlayer then
+	local hitHumanoid = didHitHumanoid(hit)
+
+	if canDamage and hitHumanoid then
+		canDamage = false
+		local hitPlayer = Players:GetPlayerFromCharacter(hitHumanoid)
+		if hitPlayer then
             GoldenHammerDamageEvent:FireServer(hitPlayer, Player)
-        end
-    end
+		end
+	end
 end
 
 -- Local function to play animation and handle hit event when tool is activated
 local function onActivated()
-    if not debounce then
-        debounce = true
-        canDamage = true
-        
-        -- play the swing track
-        swingTrack:Play()
-                
-      -- connect the event and store the connection object
-        local connection
+	if not debounce then
+		debounce = true
+		canDamage = true
 
-        connection = goldenHammer.Handle.Touched:Connect(function(hit)
-            onHit(hit)
-            connection:Disconnect()  -- Disconnect the event after it's triggered once
-        end)
+		-- play the swing track
+		if not swingTrack.IsPlaying then
+			swingTrack:Play()
+		end
 
-        -- stop playing the swing track
-        swingTrack.Stopped:Wait()
-        
-        canDamage = false
-        task.wait(0.5)  -- add a short delay before allowing another swing
-        debounce = false
-    end
+		-- connect the event and store the connection object
+		local connection
+
+		connection = hammerHead.Touched:Connect(function(hit)
+			onHit(hit)
+			connection:Disconnect()  -- Disconnect the event after it's triggered once
+		end)
+
+		-- stop playing the swing track
+		swingTrack.Stopped:Wait()
+
+		canDamage = false
+		task.wait(0.5)  -- add a short delay before allowing another swing
+		debounce = false
+	end
 end
 
 -- Event Bindings --
