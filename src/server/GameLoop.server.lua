54034f691396d6ce95e66fb98c22abff9109a1a0
anj0la <anjola.aina@gmail.com>
2024-07-05 10:40:25 -0600

    renamed roundmanager to gamemanager.

--- src/server/GameLoop.server.lua
+++ src/server/GameLoop.server.lua
@@ -5,18 +5,18 @@ local ServerScriptService = game:GetService('ServerScriptService')
 local GameModules = ServerScriptService.Server:WaitForChild('GameModules')
 
 -- Module Scripts
-local RoundManager = require(GameModules:WaitForChild('RoundManager'))
+local GameManager = require(GameModules:WaitForChild('GameManager'))
 
 -- Initialization (adding required remote events)
-RoundManager.init()
+GameManager.init()
 print('game has been initialized')
 
 -- Main game loop
 while task.wait() do
-	RoundManager.waitForPlayers()
-    RoundManager.runIntermission()
-    RoundManager.prepareRound()
-	RoundManager.runPlayerHeadstart()
-	RoundManager.runRound()
-	RoundManager.resetRound()
+	GameManager.waitForPlayers()
+    GameManager.runIntermission()
+    GameManager.prepareRound()
+	GameManager.runPlayerHeadstart()
+	GameManager.runRound()
+	GameManager.resetRound()
 end
